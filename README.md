efc1e0f80f7482ce29c7f7b960c09da021782539
Josh Pollock <jpollock412@gmail.com>
2022-12-15 13:25:07 -0500

    Fix readme and make zip

--- README.md
+++ README.md
@@ -3,9 +3,20 @@
 
 ## Develop
 
+- Install
+	- `npm i`
+	- Installs with npm and composer
+- Start environment
+	- `npm env start`
+
 - Test PHP
- -`npm run test:php`
+	- `npm run test:php`
 - Format all
- - `npm run format`
-- Build JS/css
- - `npm run build`
+	- `npm run format`
+- Build JS/CSS
+	- `npm run build`
+- Create zip
+	- `npm run zip`
+	- Installs with composer optimized
+	- Builds CSS/JS/Blocks
+	- Makes a new zip
---
+++ makeZip.js
@@ -0,0 +1,32 @@
+var fs = require('fs');
+var archiver = require('archiver');
+const slug = 'content-machine-plugin';
+var output = fs.createWriteStream(`${slug}.zip`);
+var archive = archiver('zip');
+
+console.log( 'Zipping!')
+output.on('close', function () {
+    console.log('Zipped!');
+    console.log(archive.pointer() + ' total bytes');
+});
+
+archive.on('error', function(err){
+    throw err;
+});
+
+archive.pipe(output);
+
+archive.append(fs.createReadStream(
+    __dirname + `/${slug}.php`
+), { name: `/${slug}.php` });
+
+//Directories to copy
+['php', 'build', 'vendor/composer'].forEach( ( dir ) => {
+	archive.directory(dir, '/' + dir);
+});
+
+archive.append(fs.createReadStream(
+    __dirname + '/vendor/autoload.php'
+), { name: 'vendor/autoload.php' });
+
+archive.finalize();
