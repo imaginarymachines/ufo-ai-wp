2da02bfae7a3ac8437d873bfc6fd9d01083db0d5
Josh Pollock <jpollock412@gmail.com>
2022-12-30 10:48:01 -0500

    format

--- src/settings/index.js
+++ src/settings/index.js
@@ -2,43 +2,33 @@ import { render } from 'react-dom';
 
 import React from 'react';
 
-import {
-	Tabs,
-
-} from '@imaginary-machines/wp-admin-components';
+import { Tabs } from '@imaginary-machines/wp-admin-components';
 
 import SettingsForm from './settingsTab';
 const tabs = [
 	{
 		children: <div>Tab One Content</div>,
 		id: 'welcome',
-		label: 'Welcome'
+		label: 'Welcome',
 	},
 	{
-		children: (<SettingsForm />),
+		children: <SettingsForm />,
 		id: 'settings',
-		label: 'Settings'
+		label: 'Settings',
 	},
 	{
 		children: <div>Tab Three Content</div>,
 		id: 'account',
-		label: 'Account'
-	}
+		label: 'Account',
+	},
 ];
 
 const USE_TABS = false;
 const App = () => {
-	if( ! USE_TABS ) {
-		return (
-			<SettingsForm />
-		);
+	if ( ! USE_TABS ) {
+		return <SettingsForm />;
 	}
-	return (
-		<Tabs
-			initialTab="settings"
-			tabs={tabs}
-		/>
-	)
+	return <Tabs initialTab="settings" tabs={ tabs } />;
 };
 
 render( <App />, document.getElementById( 'ufo-ai-settings' ) );
