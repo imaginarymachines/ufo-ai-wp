d9914f14c22ad0ff59f6e37686536864346b85fd
Josh Pollock <jpollock412@gmail.com>
2023-01-30 13:06:58 -0500

    Remove post route

--- php/Api/Proxy.php
+++ php/Api/Proxy.php
@@ -28,43 +28,7 @@ class Proxy {
 	 * @return self
 	 */
 	public static function factory() {
-		$obj = new static( UfoAi::getClient() );
-		\register_rest_route(
-			self::NAMESPACE,
-			'/post',
-			array(
-				'methods'             => 'POST',
-				'callback'            => array( $obj, 'forPost' ),
-				'permission_callback' => array( $obj, 'authorize' ),
-				'args'                => array(
-					'title'      => array(
-						'required' => true,
-						'type'     => 'string',
-					),
-					'categories' => array(
-						'required' => false,
-						'type'     => 'array',
-					),
-					'tags'       => array(
-						'required' => false,
-						'type'     => 'array',
-					),
-					'post'       => array(
-						'required' => true,
-						'type'     => 'integer',
-					),
-					'length'     => array(
-						'required' => false,
-						'type'     => 'integer',
-					),
-					'what'       => array(
-						'required' => false,
-						'type'     => 'string',
-						'default'  => PromptRequest::getDefaultWhat(),
-					),
-				),
-			)
-		);
+		$obj = new static( UfoAi::getClient() )
 
 		\register_rest_route(
 			self::NAMESPACE,
@@ -132,88 +96,7 @@ class Proxy {
 
 		return $this->client->text( $prompt, $temperature );
 	}
-	/**
-	 * Create a prompt from a post
-	 */
-	public function forPost( $request ) {
 
-		$what = $request->get_param( 'what' );
-		//reset to default if invalid
-		if ( ! in_array( $what, PromptRequest::getValidWhats() ) ) {
-			$what = PromptRequest::getDefaultWhat();
-		}
-
-		$post_id      = $request->get_param( 'post' );
-		$categories   = $request->get_param( 'categories' );
-		$tags         = $request->get_param( 'tags' );
-		$title        = $request->get_param( 'title' );
-		$length       = (int) $request->get_param( 'length', 1 );
-		$that         = array(
-			'about' => '',
-		);
-		$for          = array(
-			'title' => $title,
-			'type'  => 'blog post',
-		);
-		$aboutPattern = ' %s,';
-
-		if ( empty( $categories ) ) {
-			$categories = \get_the_category( $post_id );
-		} else {
-			$categories = array();
-			foreach ( $categories as $id ) {
-				$categories[] = get_category( $id );
-			}
-		}
-		if ( ! empty( $categories ) ) {
-			foreach ( $categories as $category ) {
-				if ( $category ) {
-					$that['about'] .= sprintf(
-						$aboutPattern,
-						$category->name
-					);
-				}
-			}
-		}
-		if ( empty( $tags ) ) {
-			$tags = get_the_tags( $post_id );
-		} else {
-			$tags = array();
-			foreach ( $tags as $id ) {
-				$tags[] = get_tag( $id );
-			}
-		}
-		if ( ! empty( $tags ) ) {
-			foreach ( $tags as $tag ) {
-				if ( $tag ) {
-					$that['about'] .= sprintf(
-						$aboutPattern,
-						$tag->name
-					);
-				}
-			}
-		}
-		if ( empty( $that['about'] ) ) {
-			$that['about'] = get_the_excerpt( $post_id );
-		}
-		$that['about'] = trim( $that['about'], ',' );
-		if ( empty( $that['about'] ) ) {
-			$that['about'] = $title ? $title : 'Something';
-		}
-
-		$promptRequest = new PromptRequest(
-			$what,
-			$for,
-			$that,
-			$length
-		);
-		try {
-			$texts = $this->client->prompt( $promptRequest );
-			return array( 'texts' => $texts );
-		} catch ( \Throwable $th ) {
-			return new \WP_Error( 'ufo-ai-error', $th->getMessage() );
-		}
-	}
 
 
 	/**
